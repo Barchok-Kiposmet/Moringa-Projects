8b64e81f8b3ec265114eb089dcf53d63324c341b
Patrick Barchok Kiposmet <barchokpatrick@gmail.com>
2022-10-19 18:07:38 +0300

    added the logical expressions

--- Week-2/Login.js
+++ Week-2/Login.js
@@ -1,4 +1,17 @@
 const = login() => {
     const emailElement = document.getElementById('email')
     const passwordElement = document.getElementById('password')
+
+    // console.log('Email Element', emailElement)
+    // console.log ('Password Element', passwordElement)
+
+    const email = (emailElement)
+    const password = (passwordElement)
+    // inserting logical conditions
+if (emailElement == ' ') {
+    alert ('Provide a valid email')
+} 
+if (passwordElement == ' ') {
+    alert ('Please provide a password')
+}
 }
