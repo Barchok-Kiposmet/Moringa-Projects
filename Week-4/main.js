e025eaaf9eb14b2aca75f59d92c77fe0b3281bd1
Patrick Barchok Kiposmet <barchokpatrick@gmail.com>
2022-11-03 13:48:26 +0300

    Added new files

--- Week-4/main.js
+++ Week-4/main.js
@@ -1,1 +1,35 @@
-jQuery();
+
+// instead of repeating jQuery we substitute this to the $ sign for all
+
+$(document).ready(function(){
+
+    $(window).resize(function(){
+        alert("This window has been resized");
+    });
+
+    $("#btn-one").click(function(){
+        alert("Do Something");
+    });
+
+    $("#btn-two").click( function(){
+        $(this).text("I wanna be solid!!!");
+    });
+
+    $("#btn-four").click( function(){
+        alert("Ah, there you are");
+    });
+
+    $("#btn-five").click( function(){
+        $(this).text("Updated");
+    });
+
+    $("#btn-six").click( function(){
+        $(this).text("Updated");
+    });
+
+});
+
+
+
+
+// $(selector).userAction(callBack);
