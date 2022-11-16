a1cf8f27c8fed93c3b063006f7d497a596cce7d4
Patrick Barchok Kiposmet <barchokpatrick@gmail.com>
2022-11-16 23:58:10 +0300

    new working files

---
+++ Week-3/practice/main.js
@@ -0,0 +1,39 @@
+
+// instead of repeating jQuery we substitute this to the $ sign for all
+
+$(document).ready(() => {
+
+    $(window).resize(function(){
+        alert("This window has been resized");
+    });
+
+    $("#btn-one").click(function(){
+        alert("Do Something");
+    });
+
+    $("#btn-two").click( function(){
+        $(this).text("This is the new Jquery text");
+    });
+
+    $("#btn-four").click( function(){
+        alert("Ah, there you are!");
+    });
+
+    $("#btn-five").click( function(){
+        $(this).text("Updated");
+    });
+
+    $("#btn-six").click(() => {
+        console.log('This has been changed with new code')
+    });
+
+    $('#btn-six').click(() => {
+        alert ('DO NOT DO THAT!!');
+    });
+
+    
+});
+
+
+
+// $(selector).userAction(callBack);
