8d05c89356c526705497b89b98cfc2fad848d796
Patrick Barchok Kiposmet <barchokpatrick@gmail.com>
2022-11-17 10:16:23 +0300

    removed email less than condition

--- Week-3/practice/login-form.js
+++ Week-3/practice/login-form.js
@@ -8,14 +8,17 @@
                 let message;
 
                 if (emailValue == ' ' || passwordValue == ' ') message = 'Please provide a valid email and/or password';
+                   
+                    else { if (!String(emailValue).includes('@')) message = 'Please provide a valid email';
                  
-                    else if (String(emailValue).length < 5) message = 'please provide a valid email and/or password';
+                        // else if (String(emailValue).length < 5) message = 'please provide a valid email and/or password';
                     
-                        else if (String(passwordValue).length < 5) message = 'please provide a password more than 5 characters';
+                            else if (String(passwordValue).length < 5) message = 'please provide a password of more than 5 characters';
+                                
+                                else message = 'Logged in Successfully as'+ ' ' + emailValue; 
+                    }
                         
-                            else if (!String(emailValue).includes('@')) message = 'Please provide a valid email';
                             
-                                else message = 'Logged in Successfully as' + emailValue; 
 
 
                 $('p#loginresult').text(message);
