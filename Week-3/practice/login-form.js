d7022489ad916b652ff8a678ba510d61d768d3a3
Barchok-Kiposmet <barchokpatrick@gmail.com>
2022-12-11 14:06:37 +0300

    new update to log in form

--- Week-3/practice/login-form.js
+++ Week-3/practice/login-form.js
@@ -1,34 +1,28 @@
+$(document).ready(() => {
+  $("button#loginbutton").click(() => {
+    const emailValue = $("input#emailbox").val();
+    const passwordValue = $("input#passwordbox").val();
 
-        $(document).ready(() => {
-           
-            $('button#loginbutton').click(()=>{
+    let message; // this will be replaced later at text here and is dynamic
 
-                const emailValue = $('input#emailbox').val();
-                const passwordValue = $('input#passwordbox').val();
+    // performing logical checks on inputs
 
-                let message; // this will be replaced later at text here and is dynamic
+    if (emailValue == " " || passwordValue == " ")
+      message = "Please provide a valid email and/or password";
+    else {
+      if (!String(emailValue).includes("@"))
+        message = "Please provide a valid email";
+      // else if (String(emailValue).length < 5) message = 'please provide a valid email and/or password';
+      else if (String(passwordValue).length < 5)
+        message = "please provide a password of more than 5 characters";
+      else message = "Logged in Successfully as" + " " + emailValue;
+    }
 
-                // performing logical checks on inputs
+    // return the message at results here
 
-                if (emailValue == ' ' || passwordValue == ' ') message = 'Please provide a valid email and/or password';
-                   
-                    else { if (!String(emailValue).includes('@')) message = 'Please provide a valid email';
-                 
-                        // else if (String(emailValue).length < 5) message = 'please provide a valid email and/or password';
-                    
-                            else if (String(passwordValue).length < 5) message = 'please provide a password of more than 5 characters';
-                                
-                                else message = 'Logged in Successfully as'+ ' ' + emailValue; 
-                    }
-                        
-                    // return the message at results here
+    $("p#loginresult").text(message);
+  });
 
-                $('p#loginresult').text(message);
-                            
+  event.preventDefault();
+});
 
-            });
-
-        
-        event.preventDefault();    
-
-        });
