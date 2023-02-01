dd0184e65d68f9c9495ac1c8cb30dcf21ab3d949
Barchok-Kiposmet <barchokpatrick@gmail.com>
2023-02-01 20:05:24 +0300

    added the new app

--- Week-5/index.js
+++ Week-5/index.js
@@ -1,1 +1,34 @@
-console.log('Here');
+    //Imports
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const blogRoutes = require('./routes/blog');
+
+
+const PORT = process.env.PORT;
+
+//connect app to mongodb
+const connectionString = process.env.DATABASE_URL;
+mongoose.connect(connectionString);
+
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.on('connected', () => {
+    console.log('Database connected');
+});
+
+// Create an express app
+const app = express();
+app.use('/blog', blogRoutes);
+
+
+app.listen(PORT, () => {
+    console.log('My server is running on PORT ${PORT}');
+});
+
+
