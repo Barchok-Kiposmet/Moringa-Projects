6084f1c22268e1748392baac5974836052a11527
Barchok-Kiposmet <barchokpatrick@gmail.com>
2023-03-02 15:41:39 +0300

    Curl local host

--- Week-6-Webhooks/index.js
+++ Week-6-Webhooks/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
-const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    
+    res.status(200).json({ message: 'success'})
+});
 
 app.listen(PORT, () => {
     console.log('My server is running on PORT ${PORT}');
