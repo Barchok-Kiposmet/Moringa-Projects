bc0a4d3e0d413147c41ff079af2feed8483d664c
Barchok-Kiposmet <barchokpatrick@gmail.com>
2023-03-02 10:03:31 +0300

    Started server

--- Week-6-Webhooks/index.js
+++ Week-6-Webhooks/index.js
@@ -1,1 +1,9 @@
-const express = require(express);
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('My server is running on PORT ${PORT}');
+});
