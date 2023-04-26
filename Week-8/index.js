25d9bdd803ddf57f31b0edf1525be67bccc8914e
Barchok-Kiposmet <barchokpatrick@gmail.com>
2023-04-26 11:01:38 +0300

    Test samples updated

--- Week-8/index.js
+++ Week-8/index.js
@@ -1,12 +1,17 @@
+const { json } = require('body-parser');
 const express = require('express');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).json({ running: true });
   });
 
 app.listen(PORT, () => {
-    console.log("The server is running");
+    console.log(`The server is running on localhost:${PORT}`);
 })
+
+module.exports = server;
